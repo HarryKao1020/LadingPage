67e18815ff0642a9961ff46af12e2956ce03fea7
harrykao <knight65210@gmail.com>
2020-07-09 22:52:20 +0800

    add smoke animation

--- js/app.js
+++ js/app.js
@@ -20,4 +20,27 @@ $(".burger").click(function(){
 
 
 
+
+
+
+
 // navSlide();
+
+
+// ------ canvas --------
+(function () {
+    const canvas = document.querySelector('.bg-canvas');
+    const ctx = canvas.getContext('2d');
+    const video = document.querySelector('.bg-video');
+
+    video.addEventListener('play', draw);
+		
+    function draw() {
+        canvas.width = canvas.clientWidth;
+        canvas.height = canvas.clientHeight;
+        if (video.paused || video.ended) return false;
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+        requestAnimationFrame(draw);
+    }
+})();
